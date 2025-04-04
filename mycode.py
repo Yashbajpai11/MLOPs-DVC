23158bb2109d73af35d99b0191192abab9fbca43
yashbajpai11 <ramanbajpai2004@gmail.com>
2025-04-04 20:37:37 +0530

    first commit origin main

---
+++ mycode.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+data = {
+    'name': ['yash', 'varsha', 'aishwarya'],
+    'Age': [22, 23, 22],  # Age column ki length sabhi rows ke liye match honi chahiye
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+# Directory banana
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# CSV file ka path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# CSV file save karna
+df.to_csv(file_path, index=False)
+
+print(f'CSV file saved to {file_path}')
+
